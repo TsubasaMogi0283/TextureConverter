c1d8b5e0313d76c1f7c684ecbea837a28adca142
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-09-30 15:07:26 +0900

    確認課題の途中

--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -2,9 +2,16 @@
 #include <cstdlib>
 #include <iostream>
 
-int main() {
-	std::cout << "Hello World" << std::endl;
+//ここに入っているargc,argvをコマンドライン引数と呼ぶ
+int main(int argc,char*argv[]) {
 
+	for (int i = 0; i < argc; i++) {
+		//文字列argvのi番を表示
+		std::cout << argv[i] << std::endl;
+	}
+
+	//リターンする前にpauseを入れると良いよ！
+	//ログの確認がしやすいから
 	system("pause");
 	return 0;
 }
